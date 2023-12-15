17ff9099e9c8a9de0fbe004d0d5ecf657db5384a
Julian Goede <julian.goede@smartclip.tv>
2023-12-15 13:29:10 +0100

    debug: Allow custom formats

--- debug/src/lib.rs
+++ debug/src/lib.rs
@@ -1,9 +1,7 @@
 use proc_macro::TokenStream;
-use proc_macro2;
+use proc_macro2::{self, Ident};
 use quote::quote;
-use syn::{
-    parse_macro_input, Data::Struct, DeriveInput, Fields,
-};
+use syn::{parse_macro_input, Data::Struct, DeriveInput, Fields};
 
 fn parse_fields(data: syn::DataStruct) -> Vec<syn::Field> {
     if let Fields::Named(named_fields) = data.fields {
@@ -12,26 +10,101 @@ fn parse_fields(data: syn::DataStruct) -> Vec<syn::Field> {
             .iter()
             .map(|named_field| named_field.clone())
             .collect();
+    } else {
+        Vec::new()
     }
-    return Vec::new();
 }
 
-fn fields_to_idents(fields: Vec<syn::Field>) -> Vec<proc_macro2::Ident> {
-    fields
+fn get_debug_attr(meta: &syn::Meta) -> Option<syn::ExprLit> {
+    match meta {
+        syn::Meta::NameValue(meta_name_value) => {
+            let ident = meta_name_value.path.get_ident();
+            if ident.is_none() || ident.unwrap() != "debug" {
+                return None;
+            }
+            if let syn::Expr::Lit(fmt) = &meta_name_value.value {
+                Some(fmt).cloned()
+            } else {
+                None
+            }
+        }
+        _ => None,
+    }
+}
+
+fn get_fields_with_fmt(parsed_fields: Vec<syn::Field>) -> Vec<(syn::Ident, Option<String>)> {
+    parsed_fields
         .iter()
-        .filter(|f| f.ident.is_some())
-        .map(|f| f.ident.clone().unwrap())
+        .map(|field| {
+            let attr_fmt: Option<syn::LitStr> = field
+                .attrs
+                .iter()
+                .map(|attr| get_debug_attr(&attr.meta))
+                .filter_map(|fmt_maybe| {
+                    if let Some(fmt) = fmt_maybe {
+                        match fmt.lit {
+                            syn::Lit::Str(s) => Some(s.clone()),
+                            _ => None,
+                        }
+                    } else {
+                        None
+                    }
+                })
+                .collect::<Vec<syn::LitStr>>()
+                .first()
+                .cloned();
+            (field.ident.clone(), attr_fmt)
+        })
+        .filter(|(ident, _)| ident.is_some())
+        .map(|(ident, custom_fmt)| match custom_fmt {
+            Some(fmt) => (ident.unwrap(), Some(fmt.value())),
+            None => (ident.unwrap(), None),
+        })
         .collect()
 }
 
-fn generate_field_setters(fields: Vec<syn::Field>) -> proc_macro2::TokenStream {
-    let field_name_idents:  Vec<proc_macro2::Ident> = fields_to_idents(fields);
-    let field_name_strings: Vec<String> = field_name_idents
-        .iter()
-        .map(|f| f.to_string())
-        .collect();
+#[allow(dead_code)]
+fn fields_to_idents(fields: Vec<syn::Field>) -> Vec<proc_macro2::Ident> {
+    fields.iter().filter_map(|f| f.ident.clone()).collect()
+}
 
-    quote!(#(.field(#field_name_strings, &self.#field_name_idents))*)
+fn generate_field_setters(fields: Vec<syn::Field>) -> Vec<proc_macro2::TokenStream> {
+    let fields_with_custom_fmt: Vec<(Ident, Option<String>)> = get_fields_with_fmt(fields);
+    let setters: Vec<proc_macro2::TokenStream> = fields_with_custom_fmt
+        .iter()
+        .enumerate()
+        .map(|(i, (field, fmt))| {
+            let field_name: String = field.to_string();
+            let setter = match fmt {
+                Some(custom_fmt) => {
+                    quote!(
+                        write!(f, " ")?;
+                        write!(f, #field_name)?;
+                        write!(f, ": ")?;
+                        write!(f, "{}", format!(#custom_fmt, &self.#field))?;
+                    )
+                }
+                None => {
+                    quote!(
+                        write!(f, " ")?;
+                        write!(f, #field_name)?;
+                        write!(f, ": \"")?;
+                        write!(f, "{}", &self.#field)?;
+                        write!(f, "\"")?;
+                    )
+                }
+            };
+            if i > 0 {
+                quote!(
+                    write!(f, ",")?;
+                    #setter
+                )
+            } else {
+                setter
+            }
+        })
+        .collect();
+    setters
 }
 
 fn generate_debug_impl(
@@ -43,16 +116,17 @@ fn generate_debug_impl(
     let code = quote!(
         impl std::fmt::Debug for #struct_ident {
             fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-                f.debug_struct(#struct_name)
-                    #field_setters
-                    .finish()
+                write!(f, #struct_name)?;
+                write!(f, " {{")?;
+                #(#field_setters)*
+                write!(f, " }}")
             }
         }
     );
     code
 }
 
-#[proc_macro_derive(CustomDebug)]
+#[proc_macro_derive(CustomDebug, attributes(debug))]
 pub fn derive(input: TokenStream) -> TokenStream {
     let ast = parse_macro_input!(input as DeriveInput);
     let struct_name = ast.ident;
@@ -68,9 +142,11 @@ pub fn derive(input: TokenStream) -> TokenStream {
 
 #[cfg(test)]
 mod tests {
+    use std::iter::zip;
+
     use super::*;
-    use proc_macro2::{Ident, Span};
-    use syn::{token::Colon, DeriveInput};
+    use proc_macro2::{Delimiter, Ident, Span};
+    use syn::{token::Colon, DeriveInput, Token};
 
     fn get_struct_tokens() -> proc_macro2::TokenStream {
         // fn get_struct_tokens() -> DeriveInput {
@@ -137,13 +213,27 @@ mod tests {
             create_field!("baz", "Integer"),
         ];
 
-        let actual_code = generate_field_setters(fields).to_string();
-        let expected_code = quote!(
-            .field("bar", &self.bar)
-            .field("baz", &self.baz)
-        )
-        .to_string();
-        assert_eq!(actual_code, expected_code);
+        let actual_setters = generate_field_setters(fields);
+        let expected_setters = vec![
+            quote!(
+                write!(f, " ")?;
+                write!(f, "bar")?;
+                write!(f, ": \"")?;
+                write!(f, "{}", &self.bar)?;
+                write!(f, "\"")?;
+            ),
+            quote!(
+                write!(f, ",")?;
+                write!(f, " ")?;
+                write!(f, "baz")?;
+                write!(f, ": \"")?;
+                write!(f, "{}", &self.baz)?;
+                write!(f, "\"")?;
+            ),
+        ];
+        for (actual_setter, expected_setter) in zip(actual_setters, expected_setters) {
+            assert_eq!(actual_setter.to_string(), expected_setter.to_string());
+        }
     }
 
     #[test]
@@ -157,6 +247,28 @@ mod tests {
         let expected = vec![
             Ident::new("foo", Span::call_site()),
             Ident::new("bar", Span::call_site()),
+        ];
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn test_field_attribute_is_parsed() {
+        let debug_str = "0b{:08b}".to_string();
+        let tokens = quote!(
+            struct Foo {
+                bar: String,
+                #[debug = #debug_str]
+                baz: Integer,
+            }
+        );
+        let ast = tokens_to_ast(tokens);
+        let data = ast_to_data_struct(ast);
+        let parsed_fields: Vec<syn::Field> = parse_fields(data);
+
+        let actual: Vec<(Ident, Option<String>)> = get_fields_with_fmt(parsed_fields);
+        let expected = vec![
+            (syn::Ident::new("bar", Span::call_site()), None),
+            (syn::Ident::new("baz", Span::call_site()), Some(debug_str)),
         ];
         assert_eq!(actual, expected);
     }
